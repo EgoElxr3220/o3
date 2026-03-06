f04d69dabff50f7afdf810d55ea82225bdd8b9ef
Lance Boylan <eg01st3220@gmail.com>
2026-03-06 14:12:34 -0700

    Add project files.

---
+++ o3/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Grade { get; set; }
+
+    public Student(string name, int grade)
+    {
+        Name = name;
+        Grade = grade;
+    }
+}
+
+public class Teacher
+{
+    public string Name { get; set; }
+    public List<Student> Students { get; set; }
+
+    public Teacher(string name, List<Student> students)
+    {
+        Name = name;
+        Students = students;
+    }
+
+    public static void AddStudent(Student student)
+    {
+
+    }
+
+    public static void DisplayStudents()
+    {
+
+    }
+}
